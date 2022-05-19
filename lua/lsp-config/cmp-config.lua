c9d4d24b5450aafbc1c35e7b07ea89928070b579
Bhushan <bhushangaykawad@gmail.com>
2022-05-19 13:57:45 +0530

    cmp config updated

--- lua/lsp-config/cmp-config.lua
+++ lua/lsp-config/cmp-config.lua
@@ -1,4 +1,10 @@
 local cmp = require("cmp")
+local luasnip = require('luasnip')
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 local lsp_symbols = {
     Text = "   (Text) ",
@@ -29,17 +35,41 @@ local lsp_symbols = {
 }
 
 cmp.setup({
-    mapping = {
-        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-        ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    mapping = cmp.mapping.preset.insert({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         ["<C-e>"] = cmp.mapping({
             i = cmp.mapping.abort(),
             c = cmp.mapping.close(),
         }),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    },
+        ["<C-y>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+        }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+    }),
 
     snippet = {
         -- REQUIRED - you must specify a snippet engine
@@ -48,8 +78,16 @@ cmp.setup({
         end,
     },
 
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+
+    min_length = 1,
+    preselect = true,
+
     formatting = {
-        format = function(entry, item)
+        format = function(_, item)
             item.kind = lsp_symbols[item.kind]
 
             return item
@@ -57,12 +95,11 @@ cmp.setup({
     },
 
     sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "nvim_lua" },
-        { name = "luasnip" },
-        { name = "path" },
-        { name = "look", keyword_length = 3 },
-        { name = "buffer", keyword_length = 3 },
+        { name = "nvim_lsp", priority = 1000 },
+        { name = "luasnip", priority = 900 },
+        { name = "path", priority = 800 },
+        { name = "look", keyword_length = 4, priority = 700 },
+        { name = "buffer", keyword_length = 4, priority = 600 },
     }),
 
     experimental = {
