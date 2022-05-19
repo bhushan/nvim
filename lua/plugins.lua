1f51a0f6ffa7b1aea371695e00fb68aceb13fa07
Bhushan <bhushangaykawad@gmail.com>
2022-05-19 20:52:23 +0530

    packer improvements and removed comment plugin for now

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,88 +1,110 @@
-return require("packer").startup(function()
-    -- packer can manage itself
-    use("wbthomason/packer.nvim")
+local fn = vim.fn
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local bootstraped = false
 
-    -- better syntax highlighting and parsing
-    use({
-        "nvim-treesitter/nvim-treesitter",
-        run = ":TSUpdate",
+if fn.empty(fn.glob(install_path)) > 0 then
+    bootstraped = fn.system({
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
     })
 
-    -- awesome status line
-    use({
-        "nvim-lualine/lualine.nvim",
-        requires = {
-            "kyazdani42/nvim-web-devicons",
-            opt = true,
-        },
-    })
+ vim.api.nvim_command("packadd packer.nvim")
+end
 
-    -- file explorer, might be useful sometimes
-    use({
-        "kyazdani42/nvim-tree.lua",
-        requires = {
-            "kyazdani42/nvim-web-devicons", -- optional, for file icon
-        },
-    })
+return require("packer").startup({
+    function(use)
+        -- packer can manage itself
+        use("wbthomason/packer.nvim")
 
-    -- file formatter same as prettier but with extra file types
-    use("sbdchd/neoformat")
+        -- better syntax highlighting and parsing
+        use({
+            "nvim-treesitter/nvim-treesitter",
+            run = ":TSUpdate",
+        })
 
-    -- easily fuzzy file search
-    use({
-        "nvim-telescope/telescope.nvim",
-        requires = {
-            {
-                "nvim-lua/plenary.nvim",
+        -- awesome status line
+        use({
+            "nvim-lualine/lualine.nvim",
+            requires = {
+                "kyazdani42/nvim-web-devicons",
+                opt = true,
             },
-            {
-                "nvim-telescope/telescope-fzf-native.nvim",
-                run = "make",
+        })
+
+        -- file explorer, might be useful sometimes
+        use({
+            "kyazdani42/nvim-tree.lua",
+            requires = {
+                "kyazdani42/nvim-web-devicons", -- optional, for file icon
             },
+        })
+
+        -- file formatter same as prettier but with extra file types
+        use("sbdchd/neoformat")
+
+        -- easily fuzzy file search
+        use({
+            "nvim-telescope/telescope.nvim",
+            requires = {
+                {
+                    "nvim-lua/plenary.nvim",
+                },
+                {
+                    "nvim-telescope/telescope-fzf-native.nvim",
+                    run = "make",
+                },
+            },
+        })
+
+        -- use telescope for code action
+        use({ "nvim-telescope/telescope-ui-select.nvim" })
+
+        -- better navigation to and from nvim to tmux
+        use("christoomey/vim-tmux-navigator")
+
+        -- LSP plugins
+        use({
+            "neovim/nvim-lspconfig",
+            "williamboman/nvim-lsp-installer",
+
+            -- { "tami5/lspsaga.nvim", branch = "nvim6.0" }, -- for now not using to see if it's worth it
+
+            "hrsh7th/nvim-cmp",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-nvim-lua",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "octaltree/cmp-look", -- dictionary lookup
+
+            "saadparwaiz1/cmp_luasnip", -- needed for auto completion and auto imports in combination with LuaSnip
+        })
+
+        -- create cool new snippets
+        use("L3MON4D3/LuaSnip")
+
+        -- git gutter
+        use("mhinz/vim-signify")
+
+        -- theme
+        use("projekt0n/github-nvim-theme")
+
+        -- Better keybindings management and shows hints
+        use("folke/which-key.nvim")
+
+        if bootstraped then
+            require("packer").sync()
+        end
+    end,
+    config = {
+        display = {
+            open_fn = function()
+                return require("packer.util").float({ border = "single" })
+            end,
         },
-    })
-
-    -- use telescope for code action
-    use({ "nvim-telescope/telescope-ui-select.nvim" })
-
-    -- better navigation to and from nvim to tmux
-    use("christoomey/vim-tmux-navigator")
-
-    -- LSP plugins
-    use({
-        "neovim/nvim-lspconfig",
-        "williamboman/nvim-lsp-installer",
-
-        -- { "tami5/lspsaga.nvim", branch = "nvim6.0" }, -- for now not using to see if it's worth it
-
-        "hrsh7th/nvim-cmp",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-nvim-lua",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "octaltree/cmp-look", -- dictionary lookup
-
-        "saadparwaiz1/cmp_luasnip", -- needed for auto completion and auto imports in combination with LuaSnip
-    })
-
-    -- create cool new snippets
-    use("L3MON4D3/LuaSnip")
-
-    -- git gutter
-    use("mhinz/vim-signify")
-
-    -- better way to comment things
-    use({
-        "numToStr/Comment.nvim",
-        config = function()
-            require("Comment").setup()
-        end,
-    })
-
-    -- theme
-    use("projekt0n/github-nvim-theme")
-
-    -- Better keybindings management and shows hints
-    use("folke/which-key.nvim")
-end)
+    },
+})
