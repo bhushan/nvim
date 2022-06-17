0fb11687daa56b93862195bddc1ed7e1136c9279
Bhushan <bhushangaykawad@gmail.com>
2022-06-18 00:07:47 +0530

    auto close nvim tree if it is last buffer

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -15,3 +15,14 @@ nvim_tree.setup({
     },
 })
 
+vim.api.nvim_create_autocmd("BufEnter", {
+    nested = true,
+    callback = function()
+        if
+            #vim.api.nvim_list_wins() == 1
+            and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil
+        then
+            vim.cmd("quit")
+        end
+    end,
+})
