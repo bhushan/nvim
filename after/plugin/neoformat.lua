b8142a1ca9659bb3f598092babd94ee68c97278b
Bhushan <bhushangaykawad@gmail.com>
2022-06-21 02:25:09 +0530

    enabled zsh files formatting

--- after/plugin/neoformat.lua
+++ after/plugin/neoformat.lua
@@ -11,3 +11,6 @@ g.neoformat_basic_format_trim = true
 
 -- Run all enabled formatters (by default Neoformat stops after the first formatter succeeds)
 g.neoformat_run_all_formatters = true
+
+-- shell formatter
+g.shfmt_opt = "-ci"
