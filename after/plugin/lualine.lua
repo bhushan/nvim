8539e03201441b5e3483dcadecaa9ce8da47df32
Bhushan <bhushangaykawad@gmail.com>
2022-07-04 00:41:38 +0530

    lualine config touch ups

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -10,14 +10,10 @@ local leftSeparator = ""
 local rightSeparator = ""
 
 if vim.o.background == "dark" then
-    transparent = {
-        bg = "#24292e",
-    }
+    transparent = { bg = "#22272e" }
 else
     -- inverting colors for light colorschemes
-    transparent = {
-        bg = "#ffffff",
-    }
+    transparent = { bg = "#ffffff" }
 end
 
 local empty = require("lualine.component"):extend()
@@ -33,27 +29,23 @@ end
 -- Put proper separators and gaps between components in sections
 local function process_sections(sections)
     for name, section in pairs(sections) do
-        local left = name:sub(9, 10) < "x"
         for pos = 1, name ~= "lualine_z" and #section or #section - 1 do
-            table.insert(section, pos * 2, {
-                empty,
-                color = transparent,
-            })
+            table.insert(section, pos * 2, { empty, color = transparent })
         end
+
         for id, comp in ipairs(section) do
             if type(comp) ~= "table" then
                 comp = { comp }
                 section[id] = comp
             end
-            comp.separator = left
-                    and {
-                        right = rightSeparator,
-                    }
-                or {
-                    left = leftSeparator,
-                }
+
+            local left = name:sub(9, 10) < "x"
+
+            comp.separator = left and { right = rightSeparator }
+                or { left = leftSeparator }
         end
     end
+
     return sections
 end
 
@@ -61,13 +53,15 @@ local function search_result()
     if vim.v.hlsearch == 0 then
         return ""
     end
+
     local last_search = vim.fn.getreg("/")
+
     if not last_search or last_search == "" then
         return ""
     end
-    local searchcount = vim.fn.searchcount({
-        maxcount = 9999,
-    })
+
+    local searchcount = vim.fn.searchcount({ maxcount = 9999 })
+
     return last_search
         .. "("
         .. searchcount.current
@@ -75,15 +69,6 @@ local function search_result()
         .. searchcount.total
         .. ")"
 end
-
--- local function modified()
---     if vim.bo.modified then
---         return "+"
---     elseif vim.bo.modifiable == false or vim.bo.readonly == true then
---         return "-"
---     end
---     return ""
--- end
 
 lualine.setup({
     options = {
@@ -119,8 +104,4 @@ lualine.setup({
         lualine_y = { search_result, "filetype" },
         lualine_z = { "%l:%c" },
     }),
-    inactive_sections = {
-        lualine_c = { "%f %y %m" },
-        lualine_x = {},
-    },
 })
