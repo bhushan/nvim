c49573eaa4a1be743c508f0a6135cb8581a2769f
Bhushan <bhushangaykawad@gmail.com>
2022-12-15 23:53:11 +0530

    more simplification

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -6,26 +6,25 @@ end
 
 treesitter_config.setup({
   -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-  ensure_installed = {"html", "css", "javascript", "php", "lua"},
-
+  ensure_installed = "all",
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
-    use_languagetree = true,
+    -- use_languagetree = true,
     disable = { "NvimTree" },
   },
 
-  indent = {
-    enable = true,
-  },
-
-  rainbow = {
-    enable = true,
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = 1000, -- Do not enable for files with more than n lines, int
-  },
+  -- indent = {
+  --   enable = true,
+  -- },
+  --
+  -- rainbow = {
+  --   enable = true,
+  --   extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+  --   max_file_lines = 1000, -- Do not enable for files with more than n lines, int
+  -- },
 })
 
-vim.wo.foldmethod = "expr"
-vim.o.foldexpr = "nvim_treesitter#foldexpr()"
-vim.wo.foldenable = false
+-- vim.wo.foldmethod = "expr"
+-- vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+-- vim.wo.foldenable = false
