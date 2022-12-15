6bd936417eeff33550cceddf4f43d44e001453fd
Bhushan <bhushangaykawad@gmail.com>
2022-12-15 22:53:15 +0530

    changed packer floats border to solid

--- init.lua
+++ init.lua
@@ -35,7 +35,7 @@ packer.init({
   display = {
     open_fn = function()
       return require("packer.util").float({
-        border = "rounded",
+        border = "solid",
       })
     end,
   },
