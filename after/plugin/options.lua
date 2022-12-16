815a1d835a340c6e2c94b4355d638f698fb46345
Bhushan <bhushangaykawad@gmail.com>
2022-12-17 00:41:46 +0530

    changed options

--- after/plugin/options.lua
+++ after/plugin/options.lua
@@ -1,36 +1,8 @@
-local o = vim.o
-local bo = vim.bo
-local wo = vim.wo
 local opt = vim.opt
 
--- vim.cmd("filetype plugin indent on")
-
-o.fileencoding = "utf-8" -- file encoding
+opt.fileencoding = "utf-8" -- file encoding
 
 opt.belloff = "all" -- never ring the bell for any reason
-
--- -- markdown file syntax highlight support
--- g.markdown_fenced_languages = {
---   "html",
---   "css",
---   "scss",
---   "sass",
---   "javascript",
---   "js=javascript",
---   "json=javascript",
---   "typescript=javascript",
---   "ts=javascript",
---   "php",
---   "c",
---   "cpp",
---   "java",
---   "xml",
---   "vim",
---   "yaml",
---   "lua",
---   "sh",
---   "bash=sh",
--- }
 
 -- opt.completeopt = { "menu", "menuone", "noselect", "noinsert" } -- Let the user decide about the autocomplete
 
@@ -38,68 +10,67 @@ opt.belloff = "all" -- never ring the bell for any reason
 -- vim.opt.shortmess:append("c")
 
 -- show whitespaces correctly
-vim.o.list = true
-vim.opt.listchars = {
+opt.list = true
+opt.listchars = {
     nbsp = "⦸", -- CIRCLED REVERSE SOLIDUS (U+29B8, UTF-8: E2 A6 B8)
     extends = "»", -- RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00BB, UTF-8: C2 BB)
     precedes = "«", -- LEFT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00AB, UTF-8: C2 AB) tab = "▷⋯", -- WHITE RIGHT-POINTING TRIANGLE (U+25B7, UTF-8: E2 96 B7) + MIDLINE HORIZONTAL ELLIPSIS (U+22EF, UTF-8: E2 8B AF)
     trail = "•", -- BULLET (U+2022, UTF-8: E2 80 A2)
 }
+opt.fillchars:append({ eob = " " }) -- remove ~ sign from gutter for no lines
 
--- -- move cursor to next line when line ends
--- o.whichwrap = "b,s,<,>,[,],h,l"
+-- move cursor to next line when line ends
+opt.whichwrap = "b,s,<,>,[,],h,l"
 
 -- split management
-o.splitbelow = true
-o.splitright = true
+opt.splitbelow = true
+opt.splitright = true
 
 -- 3: always and ONLY the last window
 -- 0: disable
-o.laststatus = 3
+opt.laststatus = 3
 
-vim.opt.termguicolors = true -- IMPORTANT:  use 256 colors when possible
+opt.termguicolors = true -- use terminal colors
 
 -- o.showtabline = 1 -- disable tabs at the top
 -- o.showmode = false
 
--- o.backup = false
--- o.writebackup = false
+opt.updatetime = 50 -- vim updatetime for UI
 
-o.updatetime = 50 -- vim updatetime for UI
+opt.clipboard = "unnamedplus" -- universal clipboard
 
-o.clipboard = "unnamedplus" -- universal clipboard
+opt.hlsearch = true -- highlight search
+opt.ignorecase = true -- always do a case-insensitive search
+opt.smartcase = true -- override ignorecase if uppercase chars present
+opt.incsearch = true -- highlight matches while searching
 
-o.hlsearch = true -- highlight search
--- o.ignorecase = true -- always do a case-insensitive search
-o.smartcase = true -- override ignorecase if uppercase chars present
-o.incsearch = true -- highlight matches while searching
+opt.scrolloff = 8 -- start scrolling window before 8 lines left
+opt.sidescrolloff = 8
 
-o.scrolloff = 8 -- start scrolling window before 8 lines left
-o.sidescrolloff = 8
+opt.mouse = "a" -- enable mouse
 
--- o.mouse = "a" -- enable mouse
-
-wo.wrap = false -- dont wrap lines
-wo.number = true -- show line numbers on gutter line
-wo.relativenumber = true -- relative line numbers on gutter line
--- wo.cursorline = true -- highlight currently selected line
+opt.wrap = false -- dont wrap lines
+opt.number = true -- show line numbers on gutter line
+opt.relativenumber = true -- relative line numbers on gutter line
+-- opt.cursorline = true -- highlight currently selected line
 opt.signcolumn = "yes:2" -- always have gutter padding
 
-o.tabstop = 4
-bo.tabstop = 4
-o.softtabstop = 4
-bo.softtabstop = 4
-o.shiftwidth = 4
-bo.shiftwidth = 4
--- o.autoindent = true
--- bo.autoindent = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
 opt.smartindent = true
 
--- convert tabs to spaces
-o.expandtab = true
-bo.expandtab = true
+opt.expandtab = true -- convert tabs to spaces
+
+opt.wildmode = "longest:full,full" -- complete first longest common match only in command mode
 
 opt.dictionary:append("/usr/share/dict/words")
+
+opt.confirm = true -- confirm to save buffer instead of showing error on :q
+
+opt.undofile = true -- keep persistent undo history
+opt.backup = true -- backup files whenever you save it
+opt.backupdir:remove(".") -- remove current directory form list to store backup
 
 -- update diagnostics icons
 local signs = {
@@ -117,7 +88,3 @@ for type, icon in pairs(signs) do
         numhl = "",
     })
 end
-
-local colors = vim.api.nvim_get_hl_by_name("Normal", true)
-local bg = string.format("#%06x", colors.background) -- convert decimal number to 6 digit hexadecimal color code
-vim.api.nvim_set_hl(0, "NonText", { bg = bg, fg = bg })
