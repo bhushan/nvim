a37a6be5f10f1ef7c66e76add60d3c4cb145ff96
Bhushan <bhushangaykawad@gmail.com>
2022-12-17 19:18:08 +0530

    added few treesitter default settings

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -6,7 +6,9 @@ end
 
 treesitter_config.setup({
     -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-    ensure_installed = "all",
+    -- Add languages to be installed here that you want installed for treesitter
+    ensure_installed = { "c", "cpp", "go", "lua", "python", "rust", "typescript", "help" },
+
     highlight = {
         -- `false` will disable the whole extension
         enable = true,
@@ -14,10 +16,60 @@ treesitter_config.setup({
         disable = { "NvimTree" },
     },
 
-    -- indent = {
-    --   enable = true,
-    -- },
-    --
+    indent = { enable = true },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "<c-space>",
+            node_incremental = "<c-space>",
+            scope_incremental = "<c-s>",
+            node_decremental = "<c-backspace>",
+        },
+    },
+    textobjects = {
+        select = {
+            enable = true,
+            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+            keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ["aa"] = "@parameter.outer",
+                ["ia"] = "@parameter.inner",
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner",
+            },
+        },
+        move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+                ["]m"] = "@function.outer",
+                ["]]"] = "@class.outer",
+            },
+            goto_next_end = {
+                ["]M"] = "@function.outer",
+                ["]["] = "@class.outer",
+            },
+            goto_previous_start = {
+                ["[m"] = "@function.outer",
+                ["[["] = "@class.outer",
+            },
+            goto_previous_end = {
+                ["[M"] = "@function.outer",
+                ["[]"] = "@class.outer",
+            },
+        },
+        swap = {
+            enable = true,
+            swap_next = {
+                ["<leader>a"] = "@parameter.inner",
+            },
+            swap_previous = {
+                ["<leader>A"] = "@parameter.inner",
+            },
+        },
+    },
     -- rainbow = {
     --   enable = true,
     --   extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
