bf23e5b4814a756312fcb1ddb64ddf0676bbf451
Bhushan <bhushangaykawad@gmail.com>
2022-12-17 23:34:02 +0530

    null ls has its own config file now

--- init.lua
+++ init.lua
@@ -78,21 +78,7 @@ use({ -- Additional text objects via treesitter
 -- use("sbdchd/neoformat")
 
 -- for formatters and linters
-use({
-    "jose-elias-alvarez/null-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
-
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.prettier,
-                null_ls.builtins.diagnostics.eslint,
-                null_ls.builtins.completion.spell,
-            },
-        })
-    end,
-})
+use("jose-elias-alvarez/null-ls.nvim")
 
 -- better commenting using treesitter
 use("numToStr/Comment.nvim")
