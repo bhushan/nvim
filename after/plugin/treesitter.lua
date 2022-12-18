4f2a3da97ff752d3b65446b25de15136f83f5941
Bhushan <bhushangaykawad@gmail.com>
2022-12-18 13:31:46 +0530

    activate rainbow using treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -71,11 +71,11 @@ treesitter_config.setup({
             },
         },
     },
-    -- rainbow = {
-    --   enable = true,
-    --   extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    --   max_file_lines = 1000, -- Do not enable for files with more than n lines, int
-    -- },
+    rainbow = {
+        enable = true,
+        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+        max_file_lines = 1000, -- Do not enable for files with more than n lines, int
+    },
 })
 
 -- vim.wo.foldmethod = "expr"
