ff38add2c5b73b2a99830de6ac9cf8af992d5408
Bhushan <bhushangaykawad@gmail.com>
2022-12-24 18:15:34 +0530

    improved php and added javascript snippets

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -10,9 +10,16 @@ luasnip.config.set_config({
 
 luasnip.add_snippets("php", {
     luasnip.parser.parse_snippet("class", "class $1\n{\n    $0\n}"),
-    luasnip.parser.parse_snippet("pubf", "public function $1($2): $3\n{\n    $0\n}"),
-    luasnip.parser.parse_snippet("prif", "private function $1($2): $3\n{\n    $0\n}"),
-    luasnip.parser.parse_snippet("prof", "protected function $1($2): $3\n{\n    $0\n}"),
+    luasnip.parser.parse_snippet("pubf", "public function $1($2): $3 {\n    $0\n}"),
+    luasnip.parser.parse_snippet("prif", "private function $1($2): $3 {\n    $0\n}"),
+    luasnip.parser.parse_snippet("prof", "protected function $1($2): $3 {\n    $0\n}"),
     luasnip.parser.parse_snippet("testt", "public function test_$1()\n{\n    $0\n}"),
     luasnip.parser.parse_snippet("testa", "/** @test */\npublic function $1()\n{\n    $0\n}"),
+})
+
+luasnip.add_snippets("javascript", {
+    luasnip.parser.parse_snippet("class", "class $1\n{\n    $0\n}"),
+    luasnip.parser.parse_snippet("pubf", "$1($2): $3 {\n    $0\n}"),
+    luasnip.parser.parse_snippet("prif", "$1($2): $3 {\n    $0\n}"),
+    luasnip.parser.parse_snippet("prof", "$1($2): $3 {\n    $0\n}"),
 })
