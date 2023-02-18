0556f06afc5f134a9aff6d260b17a356890ff1be
Bhushan <bhushangaykawad@gmail.com>
2023-02-18 19:36:40 +0530

    theme customization

---
+++ lua/user/plugins/theme.lua
@@ -0,0 +1,43 @@
+require("noirbuddy").setup({
+  colors = {
+    background = "#ffffff",
+    primary = "#EC0034",
+    secondary = "#008E0E",
+    noir_0 = "#0C3011",
+    noir_1 = "#113616",
+    noir_2 = "#184B1E",
+    noir_3 = "#206428",
+    noir_4 = "#34743B",
+    noir_5 = "#44834B",
+    noir_6 = "#4E8F55",
+    noir_7 = "#89B38E",
+    noir_8 = "#C8DCCB",
+    noir_9 = "#E8F2E9",
+    diagnostic_error = "#EC0034",
+    diagnostic_warning = "#EEB913",
+    diagnostic_info = "#89B38E",
+    diagnostic_hint = "#89B38E",
+    diff_add = "#008E0E",
+    diff_change = "#89B38E",
+    diff_delete = "#EC0034",
+  },
+  styles = {
+    italic = true,
+    bold = false,
+    underline = false,
+    undercurl = true,
+  },
+})
+
+local Color, colors, Group, groups, styles = require('colorbuddy').setup {}
+
+-- remove forground and background from split divider
+Group.new('VertSplit', nil, nil)
+
+-- makes telescope prompt better
+Group.new('TelescopeNormal',colors.noir_0, colors.noir_9)
+Group.new('TelescopeBorder',colors.noir_9, colors.noir_9)
+
+-- remove floaterm border
+Group.new('FloatermBorder',colors.noir_9, colors.noir_9)
+
