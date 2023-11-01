7b058e8a434d735f6e89e78ad2b5be512deaabda
Bhushan <bhushangaykawad@gmail.com>
2023-11-01 12:22:30 +0530

    changed config to support version 3

--- lua/user/plugins/indent-blankline.lua
+++ lua/user/plugins/indent-blankline.lua
@@ -1,15 +1,17 @@
-require("indent_blankline").setup({
-  char = "┊",
-  show_trailing_blankline_indent = false,
-  filetype_exclude = {
-    "help",
-    "terminal",
-    "dashboard",
-    "packer",
-    "mason",
-    "NvimTree",
-    "lspinfo",
-    "TelescopePrompt",
-    "TelescopeResults",
-  },
+require("ibl").setup({
+    indent = {
+        char = "┊",
+    },
+    -- show_trailing_blankline_indent = false,
+    -- filetype_exclude = {
+    --   "help",
+    --   "terminal",
+    --   "dashboard",
+    --   "packer",
+    --   "mason",
+    --   "NvimTree",
+    --   "lspinfo",
+    --   "TelescopePrompt",
+    --   "TelescopeResults",
+    -- },
 })
