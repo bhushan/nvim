4129cdf8bcef6447af740029961d1a04912afd46
Bhushan <bhushangaykawad@gmail.com>
2024-06-02 13:01:32 +0530

    removed plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -51,16 +51,6 @@ require('lazy').setup({
     end,
   },
 
-  -- split arrays and objects with gJ gS
-  {
-    'AndrewRadev/splitjoin.vim',
-    config = function()
-      vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
-      vim.g.splitjoin_trailing_comma = 1
-      vim.g.splitjoin_php_method_chain_full = 1
-    end,
-  },
-
   -- paste items with correct indentation
   { 'sickill/vim-pasta' },
 
