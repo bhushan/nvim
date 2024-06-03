0f910ec904be583442ca2636b959ac090fe697ef
Bhushan <bhushangaykawad@gmail.com>
2024-06-03 21:46:51 +0530

    php snippets added

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -121,17 +121,9 @@ require('lazy').setup({
           end
           return 'make install_jsregexp'
         end)(),
-        dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
-        },
+        config = function()
+          require('luasnip.loaders.from_snipmate').lazy_load()
+        end,
       },
       'saadparwaiz1/cmp_luasnip',
       'hrsh7th/cmp-nvim-lsp',
