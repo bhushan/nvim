83476b65e0da508fcce26580b874318dd99df9d4
Bhushan <bhushangaykawad@gmail.com>
2024-06-03 22:11:16 +0530

    changed the way we install lsp servers

--- lua/user/plugins/lspconfig.lua
+++ lua/user/plugins/lspconfig.lua
@@ -94,10 +94,6 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
 local servers = {
-  stylua = {},
-  intelephense = {},
-  jsonls = {},
-  tsserver = {},
   lua_ls = {
     settings = {
       Lua = {
@@ -117,7 +113,16 @@ local servers = {
 --  You can press `g?` for help in this menu.
 require('mason').setup()
 
-require('mason-tool-installer').setup { ensure_installed = servers }
+local ensure_installed = vim.tbl_keys(servers or {})
+
+vim.list_extend(ensure_installed, {
+  'stylua', -- Used to format Lua code
+  'intelephense',
+  'jsonls',
+  'tsserver',
+})
+
+require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
 require('mason-lspconfig').setup {
   handlers = {
