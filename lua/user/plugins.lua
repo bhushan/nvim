608c56930ac7e2b769d77bfd6cf124757e8ed9c6
Bhushan <bhushangaykawad@gmail.com>
2024-06-04 10:06:23 +0530

    neotree added

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -157,6 +157,39 @@ require('lazy').setup({
   -- Navigate seamlessly between Vim windows and Tmux panes.
   { 'christoomey/vim-tmux-navigator' },
 
+  -- Explorer
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    version = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      {
+        's1n7ax/nvim-window-picker',
+        version = '2.*',
+        config = function()
+          require('window-picker').setup {
+            filter_rules = {
+              include_current_win = false,
+              autoselect_one = true,
+              -- filter using buffer options
+              bo = {
+                -- if the file type is one of following, the window will be ignored
+                filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+                -- if the buffer type is one of following, the window will be ignored
+                buftype = { 'terminal', 'quickfix' },
+              },
+            },
+          }
+        end,
+      },
+    },
+    config = function()
+      require 'user/plugins/neotree'
+    end,
+  },
+
   -- dracula theme
   {
     'Mofiqul/dracula.nvim',
@@ -164,6 +197,7 @@ require('lazy').setup({
       vim.cmd.colorscheme 'dracula'
     end,
   },
+
   -- Status line
   {
     'nvim-lualine/lualine.nvim',
---
+++ lua/user/plugins/neotree.lua
@@ -0,0 +1,19 @@
+require('neo-tree').setup {
+  close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+  enable_git_status = false,
+  enable_diagnostics = false,
+  window = {
+    position = 'right',
+    mappings = {
+      ['\\'] = 'close_window',
+    },
+  },
+  filesystem = {
+    filtered_items = {
+      hide_dotfiles = true,
+      hide_gitignored = false,
+    },
+  },
+}
+
+vim.keymap.set('n', '\\', ':Neotree reveal<CR>', { desc = 'Open Neotree' })
