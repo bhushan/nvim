3d17994861ad8d149a00c7d599d6f34f26c93fa4
Bhushan <bhushangaykawad@gmail.com>
2024-06-05 13:29:04 +0530

    wip

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -87,3 +87,9 @@ vim.opt.sidescrolloff = 8
 
 -- ask for confirmation instead of erroring while saving file
 vim.opt.confirm = true
+
+-- Sign configuration
+vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
+vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
+vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
+vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
--- lua/user/plugins/lspconfig.lua
+++ lua/user/plugins/lspconfig.lua
@@ -137,8 +137,3 @@ require('mason-lspconfig').setup {
   },
 }
 
--- Sign configuration
-vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
-vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
-vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
-vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
