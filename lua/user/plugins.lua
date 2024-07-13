ecb3f5be86df01dce9d2f095bc367593b616274a
Bhushan <bhushangaykawad@gmail.com>
2024-07-13 15:42:59 +0530

    remove title from floatterm

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -201,6 +201,7 @@ require('lazy').setup({
       vim.g.floaterm_width = 0.8
       vim.g.floaterm_height = 0.8
       vim.g.floaterm_wintitle = 0
+      vim.g.floaterm_title = 0
 
       vim.keymap.set('n', '`', ':FloatermToggle<CR>')
       vim.keymap.set('t', '`', '<C-\\><C-n>:FloatermToggle<CR>')
