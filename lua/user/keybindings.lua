cd70274969d694aa875aa25747c83d757deb30b7
Bhushan <bhushangaykawad@gmail.com>
2025-09-24 19:06:08 +0530

    WIP

--- lua/user/keybindings.lua
+++ lua/user/keybindings.lua
@@ -65,3 +65,19 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.hl.on_yank()
   end,
 })
+
+-- Run Pint on save in PHP files
+local pint_group = vim.api.nvim_create_augroup('PintOnSave', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = '*.php',
+  command = 'silent !./vendor/bin/pint %',
+  group = pint_group,
+})
+
+-- Run Stylua on save in Lua files
+local stylua_group = vim.api.nvim_create_augroup('StyluaOnSave', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = '*.lua',
+  command = 'silent !stylua %',
+  group = stylua_group,
+})
