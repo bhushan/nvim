c4ba53af74adb6909e0a1f3d9511a76aafe27345
Bhushan <bhushangaykawad@gmail.com>
2025-09-25 10:18:52 +0530

    WIP

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -310,6 +310,37 @@ require('lazy').setup({
       require 'user/plugins/lualine'
     end,
   },
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    enabled = true,
+    opts = {},
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      -- "rcarriga/nvim-notify",
+    },
+    config = function()
+      require('noice').setup {
+        lsp = {
+          override = {
+            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+            ['vim.lsp.util.stylize_markdown'] = true,
+            ['cmp.entry.get_documentation'] = true,
+          },
+          hover = {
+            silent = true,
+          },
+        },
+        presets = {
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = true, -- add a border to hover docs and signature help
+        },
+      }
+    end,
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
