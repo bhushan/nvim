859df5f27b4d214cf5a744f3e0872cbef2d156d5
Bhushan <bhushangaykawad@gmail.com>
2025-09-25 10:30:31 +0530

    feat: added laravel and php and UI plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -302,12 +302,76 @@ require('lazy').setup({
     end,
   },
 
+  -- Laravel IDE PHPstorm plugin like support
+  {
+    'adibhanna/laravel.nvim',
+    enabled = false,
+    -- dir = "~/Developer/opensource/laravel.nvim",
+    ft = { 'php', 'blade' },
+    dependencies = {
+      'folke/snacks.nvim', -- Optional: for enhanced UI
+    },
+    config = function()
+      require('laravel').setup {
+        notifications = false,
+        debug = false,
+        keymaps = true,
+      }
+    end,
+  },
+
+  -- refactoring capabilities
+  {
+    'adibhanna/phprefactoring.nvim',
+    enabled = false,
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+    },
+    ft = 'php',
+    config = function()
+      require('phprefactoring').setup()
+    end,
+  },
+
   -- Status line
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
       require 'user/plugins/lualine'
+    end,
+  },
+
+  -- nice UI
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    enabled = true,
+    opts = {},
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      -- "rcarriga/nvim-notify",
+    },
+    config = function()
+      require('noice').setup {
+        lsp = {
+          override = {
+            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+            ['vim.lsp.util.stylize_markdown'] = true,
+            ['cmp.entry.get_documentation'] = true,
+          },
+          hover = {
+            silent = true,
+          },
+        },
+        presets = {
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = true, -- add a border to hover docs and signature help
+        },
+      }
     end,
   },
 }, {
